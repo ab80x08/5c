99b59f0442055c37796d4b570c52b7cf43fe7051
4lex8lue <ab.80x08@gmail.com>
2026-07-30 02:41:36 +0000

    r6y

---
+++ a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+  int A,B,S;
+  printf("Input 1st : ");
+  scanf("%d",&A);
+  printf("Input 2nd : ");
+  scanf("%d",&B);
+  S=A+B;
+  printf("The summation is = %d",S);
+
+  return 0;
+}
