cee1c9de269b45cfe03dbfd95201561777700813
4lex8lue <ab.80x08@gmail.com>
2026-08-22 05:07:48 +0000

    nth teem +

--- a.c
+++ a.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main ()
 {
-  int A,B,S;
-  printf("Input 1st : ");
-  scanf("%d",&A);
-  printf("Input 2nd : ");
-  scanf("%d",&B);
-  S=A+B;
-  printf("The summation is = %d",S);
-
+  int i=2,j=1,N,S=0;
+  printf("Value of N :  ");
+  scanf("%d",&N);
+  printf("The calculated terms are :  ");
+  while(j<=N)
+  {
+    S+=i;
+    i+=2;
+    ++j;
+  }
+  printf("%d\n",S);
   return 0;
 }
